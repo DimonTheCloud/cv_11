bc061cd2fccb553c45e331174bb2ed87e0820f7f
Dmytro Antonenko <dimchik41414@gmail.com>
2026-04-20 13:21:16 +0200

    rectangle

--- sorting.py
+++ sorting.py
@@ -1,29 +1,31 @@
 import random
 import matplotlib.pyplot as plt
 
+my_numbers = [1, 68, 39, 23, 49, 20, 92, 49, 29, 57]
 
-def bubble_sort(numbers):
-    n = len(numbers)
+def bubble_sort(my_list):
+
+    n = len(my_list)
 
     for i in range(n):
         for j in range(n - 1 - i):
+            if my_list[j] > my_list[j + 1]:
+                my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+
             plt.ion()
             plt.show()
 
             index_highlight1 = j
             index_highlight2 = j + 1
-            colors = ["steelblue"] * len(numbers)
+            colors = ["steelblue"] * len(my_list)
             colors[index_highlight1] = "tomato"
             colors[index_highlight2] = "tomato"
             plt.clf()
-            plt.bar(range(len(numbers)), numbers, color=colors)
+            plt.bar(range(len(my_list)), my_list, color=colors)
             plt.title("Bubble Sort")
             plt.pause(0.1)
 
-            if numbers[j] > numbers[j + 1]:
-                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
-
-    return numbers
+    return my_list
 
 
 
